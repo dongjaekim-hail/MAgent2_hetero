18cd31049a1f7e15266ec8125706e7febd32fb74
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-05-11 09:48:56 +0900

    update

---
+++ test.py
@@ -0,0 +1,23 @@
+from magent2.environments import adversarial_pursuit_v4
+
+render_mode = 'human'
+# render_mode = 'rgb_array'
+env = adversarial_pursuit_v4.env(map_size=45, minimap_mode=False, tag_penalty=-0.2,
+max_cycles=500, extra_features=False, render_mode=render_mode)
+
+for ep in range(1000):
+	env.reset()
+
+	for agent in env.agent_iter():
+
+		observation, reward, termination, truncation, info = env.last()
+
+		if termination or truncation:
+			print(agent, ' is terminated')
+			env.step(None) # need this
+			continue
+		else:
+			action = env.action_space(agent).sample()
+			env.step(action)
+
+	# env.state() # receives the entire state
