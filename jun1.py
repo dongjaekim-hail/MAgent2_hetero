ab636738895233be22463a82f5cce14648289222
junhyeonbae <bjhdo0807@gmail.com>
2023-08-15 16:41:18 +0900

    "version5"

---
+++ jun1.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.path)
+
+#
+# ['/Users/junhyeonbae/PycharmProjects/MAgent2_hetero',
+#  '/Users/junhyeonbae/PycharmProjects/MAgent2_hetero',
+#  '/Users/junhyeonbae/anaconda3/envs/name/lib/python38.zip',
+#  '/Users/junhyeonbae/anaconda3/envs/name/lib/python3.8',
+#  '/Users/junhyeonbae/anaconda3/envs/name/lib/python3.8/lib-dynload',
+#  '/Users/junhyeonbae/anaconda3/envs/name/lib/python3.8/site-packages']
+#
+#
+# #numpy가 설치된 경로
+# /Users/junhyeonbae/anaconda3/envs/MAgent2_hetero/lib/python3.8/site-packages
